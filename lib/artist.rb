b8ac13803bb30a31b98c2ceb0e8a2a9c5c595560
Carlos Barrera <barrera9696@gmail.com>
2019-05-15 15:41:18 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Song 
+    attr_accessor :title 
+    def initialize title
+        @title = title 
+    end 
+end 
+
+class Artist
+    attr_accessor :name
+    def initialize
+        @name = name 
+    end 
+end 
